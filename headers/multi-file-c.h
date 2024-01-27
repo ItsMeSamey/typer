e0330e0961f8fd92fefccf84e335a9d12e1a40a6
ItsMeSamey <sameychain5041@gmail.com>
2024-01-27 12:13:43 +0530

    Minor improvements

--- headers/multi-file-c.h
+++ headers/multi-file-c.h
@@ -2,6 +2,10 @@
 
 #include <cstring>
 #ifndef __cplusplus // if !c++
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
 #endif // end if !c++
 
 #ifdef __cplusplus // if c++
@@ -11,18 +15,17 @@
 #include <cassert>
 #endif // end if c++
 
-
 typedef struct multi_file_struct {
   void* data = NULL;/*stores filename/ data to be written*/
   void* next = NULL;/*stores indices array/ pointer to next element*/
 } multi_file_struct;
 
-void multi_file_write_oprations(multi_file_struct *STRUCT, uint64_t OPERATION = 0, void * DATA = NULL, uint64_t INDEX = 0,uint64_t len = 0){
+void multi_file_write_oprations(multi_file_struct *STRUCT, uint64_t OPERATION = 0, void * DATA = NULL, uint64_t INDEX = 0, uint64_t len = 0){
   const uint64_t INIT=0, FREEABLE=1, NONFREE=2, DEINIT=3;
   uint64_t i, tell;
   FILE *multi_file;
   multi_file_struct *_cur, *cur;
-  char *buff;
+  uint64_t *buff;
   switch(OPERATION){
     case INIT:
       for (i = 0; i < INDEX; i++){
@@ -52,7 +55,7 @@ void multi_file_write_oprations(multi_file_struct *STRUCT, uint64_t OPERATION = 
       cur = (multi_file_struct*)_cur->next;
       cur->data = (void*)DATA;
       cur->next = malloc(sizeof(multi_file_struct) + sizeof(size_t) + 1);
-      *(uint64_t *)(((size_t)_cur->next) + sizeof(multi_file_struct)) = strlen((char*)DATA);
+      *(uint64_t *)(((size_t)_cur->next) + sizeof(multi_file_struct)) = len ? len :strlen((char*)DATA);
       _cur->next = cur->next;
       *(uint8_t *)((char*)cur->next + sizeof(multi_file_struct) + sizeof(size_t)) = 0;
       ((multi_file_struct*)cur->next)->next = NULL;
@@ -60,15 +63,14 @@ void multi_file_write_oprations(multi_file_struct *STRUCT, uint64_t OPERATION = 
     case DEINIT:
       multi_file = fopen((char*)STRUCT->data, "w");
       assert(multi_file);
-      fprintf(multi_file, "%p", (void*)(*(uint64_t*)STRUCT->next));
-      buff = (char *)malloc(sizeof(char) * 36L*(*(uint64_t*)STRUCT->next));
-//       fwrite(buff, sizeof(char), 36L*(*(uint64_t*)STRUCT->next), multi_file);
-      fseek(multi_file, 18L+36L*(*(uint64_t*)STRUCT->next),SEEK_CUR);
+      fwrite((uint64_t*)STRUCT->next, sizeof(uint64_t), 1, multi_file);
+      fseek(multi_file, sizeof(uint64_t), SEEK_SET);
+      buff = (uint64_t *)malloc(sizeof(uint64_t) * (*(uint64_t*)STRUCT->next));
+
       for (i = 0; i < *(uint64_t*)STRUCT->next; i++){
         _cur = &((multi_file_struct*)(2*sizeof(uint64_t)+(char*)STRUCT->next))[i];
         cur = (multi_file_struct*)(_cur->data);
-        sprintf((buff + 36L*i), "%p", (void*)(i+1));
-        sprintf((buff + 36L*i+18L), "%p", (void*)ftell(multi_file));
+        buff[i] = ftell(multi_file);
         while (cur != _cur->next){
           fwrite(cur->data, sizeof(char), *(size_t*)(((size_t)cur) +sizeof(multi_file_struct)), multi_file);
           if ( *(uint8_t *)(((size_t)cur) +sizeof(multi_file_struct) + sizeof(size_t)) ) free(cur->data);
@@ -77,22 +79,37 @@ void multi_file_write_oprations(multi_file_struct *STRUCT, uint64_t OPERATION = 
           free(to_be_freed);
         }
       }
-      fseek(multi_file,18L,SEEK_SET);
-      fwrite(buff, sizeof(char), 36L*(*(uint64_t*)STRUCT->next), multi_file);
+      fwrite(buff, sizeof(uint64_t), (*(uint64_t*)STRUCT->next), multi_file);
+      free(buff);
+      free(_cur->next);
       fclose(multi_file); /* close the file */
       break;
     default: assert(!"operation was invalid");
   }
 }
+char **multi_file_write_oprations(char * file_name){
+  FILE *multi_file = fopen(file_name, "r");
+  assert(multi_file);
+  if (!multi_file) return NULL;
+  uint64_t num;
+  fseek(multi_file,0L,SEEK_SET);
+  fread(&num, sizeof(uint64_t), 1, multi_file);
 
+  fseek(multi_file, -num,SEEK_END);
+  const size_t size = ftell(multi_file);
+  char *buff = (char *)malloc(size + (num+2) *sizeof(uint64_t));
+  fseek(multi_file, sizeof(uint64_t), SEEK_SET);
+  fread(buff, sizeof(char), size, multi_file);
 
+  *(void**)(buff+size) = buff;
+  void **start = ((void**)(buff+size)) + 1;
+  fseek(multi_file, -num,SEEK_END);
+  fread(start, sizeof(uint64_t), num, multi_file);
 
-void write_file_and_stuff(char * file_name){
-  size_t index = 0;
-
-//   fseek(options_file,0L,SEEK_SET);
-//   fread(buff, 1, length_of_file, options_file);
+  for (uint64_t i = 0; i < num; i++){start[i] = buff + ((uint64_t*)start)[i];}
+  start[num] = NULL;
+  fclose(multi_file); /* close the file */
+  return (char **)start;
 }
-
 
 
--- headers/test.cpp
+++ headers/test.cpp
@@ -8,11 +8,10 @@ void parg(char *a, char **v, size_t t1, size_t t2) {
 int main(int argc, char **argv) {
   multi_file_struct S;
   multi_file_write_oprations(&S, 0, (void*)"1.txt", 1);
-  multi_file_write_oprations(&S, 2, (void*)"HELLO M ", 0);
-  multi_file_write_oprations(&S, 2, (void*)"HELLO MYGUY", 0);
-  multi_file_write_oprations(&S, 2, (void*)"This Seems To be working", 0);
-//   multi_file_write_oprations(&S, 0, (void*)"1.txt", 1);
-//   multi_file_write_oprations(&S, 2, (void*)"YES REALLLLYYYYYYYYYYY!!!!!!!!!!!!!!", 1);
+  multi_file_write_oprations(&S, 2, (void*)"HEL_LO_\xff", 0, 8);
+//   multi_file_write_oprations(&S, 2, (void*)"This Seems To be working", 0);
+  multi_file_write_oprations(&S, 0, (void*)"1.txt", 1);
+  multi_file_write_oprations(&S, 2, (void*)"!!!!", 1);
   multi_file_write_oprations(&S, 3);
   void *what;
   return 0;
